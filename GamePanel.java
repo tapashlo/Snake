efdfc7e2d028dc4aff59d2de0b60edbac3795ac2
Sandywandy <38629325+tapashlo@users.noreply.github.com>
2022-02-20 16:40:39 -0600

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,260 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+import java.awt.FontMetrics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS =(SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int cherriesEaten;
+	int cherryX;
+	int cherryY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
+	
+ 	
+	GamePanel(){
+		
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+	}
+	
+	//Method to start the game
+	
+	public void startGame() {
+		newCherry();
+		running = true;
+		timer = new Timer(DELAY,this);
+		timer.start();
+		
+		
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+		
+		
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+		if(running) {
+		
+			for (int i =0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH,i*UNIT_SIZE);
+			}
+		
+			g.setColor(Color.red);
+			g.fillOval(cherryX, cherryY, UNIT_SIZE, UNIT_SIZE);
+		
+			for (int i =0; i < bodyParts; i++) {
+			
+				if (i == 0) {
+					g.setColor(Color.green);
+					g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+				
+				}
+			
+				else {
+					g.setColor(new Color(45,180,0));
+					g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}
+			
+			g.setColor(Color.red);
+			g.setFont(new Font("Ink Free",Font.BOLD, 30));
+			FontMetrics metrics = getFontMetrics(g.getFont());
+			g.drawString("Score: "+ cherriesEaten, (SCREEN_WIDTH-metrics.stringWidth("Score: "+cherriesEaten))/2,g.getFont().getSize());
+			
+		}
+		
+		else {
+			gameOver(g);
+		}
+		
+		
+	}
+	
+	public void newCherry() {
+		
+		cherryX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		cherryY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+		
+	}
+	
+	public void move() {
+		
+		for (int i = bodyParts; i >0; i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+			
+		}
+		
+		switch (direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] +UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] -= UNIT_SIZE;
+			break;
+		case 'R':
+			x[0]+= UNIT_SIZE;
+		}
+		
+		
+		
+	}
+	
+	public void checkCherry() {
+		if((x[0] == cherryX) && (y[0] == cherryY)) {
+			bodyParts++;
+			cherriesEaten++;
+			newCherry();
+		}
+			
+		
+	}
+	
+	public void checkCollisions() {
+		
+		//checks if the head collides with the body
+		
+		for(int i = bodyParts; i>0; i--) {
+			if((x[0] == x[i])&& (y[0] == y[i])){
+				
+				running = false;
+				
+			}
+		}
+		
+		//Checks if the head touches left border!
+		
+		if (x[0] < 0) {
+			running = false;
+		}
+		//Check if the head touches right border
+		if (x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		
+		//Checks if the head touches top
+		
+		if (y[0] < 0) {
+			running = false;
+			
+		}
+		
+		//checks if the head touches the bottom
+		
+		if (y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		
+		if(!running) {
+			timer.stop();
+		}
+	}
+	
+	public void gameOver(Graphics g) {
+		//Score
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free",Font.BOLD, 30));
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Score: "+cherriesEaten, (SCREEN_WIDTH-metrics1.stringWidth("Score: "+cherriesEaten))/2,SCREEN_HEIGHT/2);
+		
+		
+		//GameOverTexts
+		
+		
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free",Font.BOLD, 75));
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH-metrics2.stringWidth("Game Over"))/2,g.getFont().getSize());
+		
+		
+		
+		
+		
+	
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		if(running) {
+			move();
+			checkCherry();
+			checkCollisions();
+			
+		}
+		repaint();
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		
+		@Override
+		public void  keyPressed(KeyEvent e) {
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction !='R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction !='L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction!='U') {
+					direction = 'D';
+				}
+				break;
+			}
+			
+			
+			
+			
+		}
+		
+	}
+	
+	
+
+}
